6c5ba6509f2d16b68ce1fe999060013525e4ab74
piotr-ole <piotrek.olesiejuk@gmail.com>
2019-12-14 15:43:34 +0100

    Base functionality for scatterplot

--- app.R
+++ app.R
@@ -1,5 +1,6 @@
 library(shiny)
-
+library(dplyr)
+library(ggthemes)
 
 
 
@@ -8,32 +9,73 @@ ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
             fileInput("file", "Choose a CSV file"),
-            checkboxInput("heat", "Heatmap", c("heatmap", "barchart", "scatterplot")),
-            uiOutput("col"),
-            uiOutput("size"),
-            
+            checkboxInput(inputId = "isHeat", label = "Heatmap"),
+            checkboxInput(inputId = "isBar", label = "Barchart"),
+            checkboxInput(inputId = "isScat", label = "Scatterplot"),
+            #uiOutput("heatmap"),
+            conditionalPanel(
+                condition = "input.isScat == true",
+                htmlOutput("scatterHeader"),
+                uiOutput("scatterUI")
+            )
+            #uiOutput("barchart"),
         ),
         mainPanel(
-            #plotOutput(outputId = "distPlot")
-            
+            plotOutput("scatterPlot")
         )
-    )
+    )   
 )
 
 server <- function(input, output) {
 
+    output$scatterHeader <- renderUI(HTML("<br><h4><b>Scatter plot</b></h4><br>"))
     
-    output$plotSpecification <- renderUI({
+    output$scatterUI <- renderUI({
         if (!is.null(input$file)) {
             f <- input$file
-            return({selectInput("col", "Color variable", c("None", colnames(read.csv(f$datapath))));
-            selectInput("col", "Color variable", c("None", colnames(read.csv(f$datapath)))) })
+            list(
+                selectInput("xScat", "x-axis variable", c("None", colnames(read.csv(f$datapath)))),
+                selectInput("yScat", "y-axis variable", c("None", colnames(read.csv(f$datapath)))),
+                selectInput("colScat", "Color variable", c("None", colnames(read.csv(f$datapath)))),
+                selectInput("sizeScat", "Size variable", c("None", colnames(read.csv(f$datapath)))),
+                actionButton("buttonScat", "Generate plot")
+            )
         }
+    })
+    observeEvent(input$buttonScat, {  
+    output$scatterPlot <- renderPlot({
+        if (input$isScat == TRUE) {
+            if (!is.null(input$file)) {
+                f <- input$file
+                dat <- read.csv(f$datapath, stringsAsFactors = TRUE)
+                dat <- dat %>% 
+                    mutate(color_ = rep("#2ca25f", nrow(dat))) %>%
+                    mutate(size_ = rep(1, nrow(dat)))
+                ggplot(data =  dat, 
+                       aes(x = eval(sym(input$xScat)),
+                           y = eval(sym(input$yScat)),
+                           col = eval(set_value(input$colScat, color_)),
+                           size = eval(set_value(input$sizeScat, size_))
+                       )) + 
+                    geom_point() + 
+                    theme_fivethirtyeight() +
+                    ggtitle(paste0("Scatter plot ", input$yScat, "~", input$xScat)) +
+                    xlab(input$xScat) +
+                    ylab(input$yScat) +
+                    theme(plot.title = element_text(hjust = 0.5), 
+                          axis.title = element_text())
+            }
+        }
+    })
     })
     
 }
 
 shinyApp(ui = ui, server = server)
 
-
-
+set_value <- function(value, standard) {
+    if (value != 'None') {
+        return(sym(value))
+    }
+    standard
+}
